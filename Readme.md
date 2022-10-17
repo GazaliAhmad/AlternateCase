8c1de2dd700dacedc7a48ac307ad8b8d8007c918
Gazali <gazali.ahmad@gmail.com>
2022-10-17 11:36:28 +0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Text Manipulation
+
+Fun text manipulation.
+
+- `alternateCase()`: A simple function that takes a string and returns a string with alternating upper and lower case characters.
+
+- `countSameLetters()`: A simple function that takes a string and returns a string with the number of times each letter appears in the string.
---
+++ src/textManipulation.java
@@ -0,0 +1,48 @@
+import org.jetbrains.annotations.NotNull;
+import static java.lang.System.out;
+
+@SuppressWarnings("ReassignedVariable")
+public class textManipulation {
+	public static void main(String[] args) {
+		var s1 = "abcdeEEgGGG";
+		var s2 = "aaabbbBBBcd";
+		var s3 = "aBBcDeFgBB";
+		var s4 = "abcd";
+		var s5 = "thequickbrownfoxjumpsoverthelazydog";
+		
+		alternateCase(s1);
+		countSameLetters(s2);
+		countSameLetters(s3);
+		countSameLetters(s4);
+		alternateCase(s5);
+		countSameLetters(s5);
+	}
+	
+	static void alternateCase(@NotNull String text) {
+		StringBuilder result = new StringBuilder();
+		int i = 0;
+		while (i < text.length()) {
+			if (i % 2 == 0) result.append(Character.toUpperCase(text.charAt(i)));
+			else result.append(Character.toLowerCase(text.charAt(i)));
+			i++;
+		}
+		out.println("\nAlternate case: " + text + " : " + result);
+	}
+	
+	static void countSameLetters(@NotNull String text) {
+		StringBuilder result = new StringBuilder();
+		var sortedText= text.chars().sorted().collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+		int i = 0;
+		while (i < text.length()) {
+			int counter = 1;
+			while (i < sortedText.length() - 1 && sortedText.charAt(i) == sortedText.charAt(i + 1)) {
+				counter++;
+				i++;
+			}
+			if (counter == 1) result.append(sortedText.charAt(i));
+			else result.append(sortedText.charAt(i)).append(counter);
+			i++;
+		}
+		out.println("\nCount each letter occurrences: " + text + " : " + result);
+	}
+}
